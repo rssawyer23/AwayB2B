a8d4072be71ff3ce5918494125de3c440a82332c
Rob Sawyer <rssawyer23@gmail.com>
2017-12-07 22:06:17 -0500

    Adjustment for prior

--- BayesianAnalysis.R
+++ BayesianAnalysis.R
@@ -1,6 +1,6 @@
 x = seq(0,1,length=1000)
-a = 695  # The number of wins observed
-b = 675  # The number of losses observed
+a = 696  # The number of wins observed
+b = 676  # The number of losses observed
 hx = dbeta(x,shape1=a,shape2=b)
 plot(x, hx, type='l',lwd=2,xlab="Theta",ylab="Theta Posterior Density",xlim=c(0.25,0.75),ylim=c(0,35))
 abline(v=0.5,col=3)
